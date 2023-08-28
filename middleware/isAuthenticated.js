05a3a9edebd3917797cdc5fc453ca34b0becf6cc
Rishabh Prajapati <rishabhprajapati411@gmail.com>
2023-08-28 17:19:49 +0530

    Added isAuthenticated file into project for future authentication logic

---
+++ middleware/isAuthenticated.js
@@ -0,0 +1,26 @@
+const jwtUtils = require("../utils/jwtUtils");
+
+exports.isAuthenticated = (req, res, next) => {
+  // Get the authorization header from the request
+  const authHeader = req.headers.authorization;
+
+  // If the header is not present, return an error message
+  if (!authHeader) {
+    return res.status(401).json({ message: "No authorization header" });
+  }
+
+  // Extract the token from the header
+  const token = authHeader.split(" ")[1];
+
+  // Verify the token using jwtUtils
+  const decoded = jwtUtils.verifyToken(token);
+
+  // If the token is invalid, return an error message
+  if (!decoded) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+
+  // If the token is valid, attach the user email to the request object and proceed to the next middleware or route handler
+  req.userEmail = decoded.email;
+  next();
+};
